1e8ce289cb10135f5017070252abf50f1b734814
Emmanuel <zacmac807@gmail.com>
2024-05-24 23:39:18 +0300

    complete task

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+
 class Dog:
     # Class body goes here
-
     #Instance method definition
     pass
+# self  =allows methods to access the other methods and attributes
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoppy = Dog()
+snoppy.bark()
+snoppy.sit()
+
